c6e4f9090a751581ae591879e8fb1513514946ee
Daniel-Ferreira-Lara <d2021003661@unifei.edu.br>
2021-11-11 15:16:47 -0300

    3344

---
+++ 3 - 3344 - Brute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   puts("4");
+   return 0; //Na prática sempre vira 4 a partir de algumas iterações
+}
+/*
+#include <stdio.h>
+void main()
+{
+   puts("4"); //Na prática sempre vira 4 a partir de algumas iteraçõe
+}*/
