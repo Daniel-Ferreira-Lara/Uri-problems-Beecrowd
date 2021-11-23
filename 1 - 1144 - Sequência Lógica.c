9c79140c28b0cd4c17598da11251b589308ae3cd
Daniel-Ferreira-Lara <d2021003661@unifei.edu.br>
2021-11-23 13:45:37 -0300

    1144

---
+++ 1 - 1144 - Sequência Lógica.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+   int num, i;
+   scanf("%d", &num);
+   for (i = 1; i < num + 1; i++)
+      printf("%d %d %d\n%d %d %d\n", i, i * i, i * i * i, i, (i * i) + 1, (i * i * i) + 1);
+   return 0;
+}
